b7f9a7e4731cc6a4ef41d20c4a0bd74a17e72c91
iscream208 <1271684870@qq.com>
2024-09-05 11:48:28 +0800

    fixed flik

--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -3,7 +3,7 @@ package flik;
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
-        for (int j = 0; i < 500; ++i, ++j) {
+        for (int j = 0; i < 500; ++i, ++j) {//-128到127java有整数缓存，地址相同，超过127是新对象，地址不同，flik的方法是==比地址
             if (!Flik.isSameNumber(i, j)) {
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,163 +1,133 @@
 package deque;
 
-import java.lang.reflect.Array;
+
 import java.util.Iterator;
 
-public class ArrayDeque<T> implements Deque<T>,Iterable<T> {
+public class ArrayDeque<T> implements Deque<T>, Iterable<T> {
     private T[] array;
     private int nextFirst;
     private int nextLast;
     private int size;
-    public ArrayDeque(){
-        array=(T[]) new Object[8];
-        nextFirst=array.length-1;
-        nextLast=0;
-        size=0;
+    public ArrayDeque() {
+        array = (T[]) new Object[8];
+        nextFirst = array.length-1;
+        nextLast = 0;
+        size = 0;
     }
-    public void addFirst(T item){
-        if (size==array.length){
+    public void addFirst(T item) {
+        if (size == array.length) {
             resize();
         }
-        array[nextFirst]=item;
+        array[nextFirst] = item;
         //System.out.println("added"+array[nextFirst]);
-        nextFirst=moveLoop(false,nextFirst);
+        nextFirst = moveLoop(false,nextFirst);
         size++;
     }
-    public void addLast(T item){
-        if (size==array.length){
+    public void addLast(T item) {
+        if (size == array.length) {
             resize();
         }
-        array[nextLast]=item;
-        nextLast=moveLoop(true,nextLast);
+        array[nextLast] = item;
+        nextLast = moveLoop(true,nextLast);
         size++;
     }
 //    public boolean isEmpty(){
 //        return size==0;
 //    }
-    public T removeFirst(){
-        if(isEmpty()){
+    public T removeFirst() {
+        if(isEmpty()) {
             return null;
         }
-        if(array.length>16&&size<array.length/4){
+        if(array.length>16&&size<array.length/4) {
             resizeShort();
         }
-        T item=array[moveLoop(true,nextFirst)];
-        array[moveLoop(true,nextFirst)]=null;
-        nextFirst=moveLoop(true,nextFirst);
+        T item = array[moveLoop(true,nextFirst)];
+        array[moveLoop(true,nextFirst)] = null;
+        nextFirst = moveLoop(true,nextFirst);
         size--;
         return item;
     }
-    public T removeLast(){
-        if(isEmpty()){
+    public T removeLast() {
+        if(isEmpty()) {
             return null;
         }
-        if(array.length>16&&size<array.length/4){
+        if(array.length>16&&size<array.length/4) {
             resizeShort();
         }
-        T item=array[moveLoop(false,nextLast)];
-        array[moveLoop(false,nextLast)]=null;
-        nextLast=moveLoop(false,nextLast);
+        T item = array[moveLoop(false,nextLast)];
+        array[moveLoop(false,nextLast)] = null;
+        nextLast = moveLoop(false,nextLast);
         size--;
         return item;
     }
-//    private void resize(){
-//        T[] newArray=(T[]) new Object[array.length*2];
-//        if (nextLast!=0){
-//            for(int i=0;i<nextLast;i++){
-//                newArray[i]=array[i];
-//            }
-//            for(int i=0;i<array.length-1-nextFirst;i++){
-//                newArray[newArray.length-1-i]=array[array.length-1-i];
-//            }
-//            nextFirst=newArray.length-1-(array.length-1-nextFirst);
-//            this.array=newArray;
-//        }
-//        else {//边界情况
-//            for (int i=0;i<size;i++){
-//                newArray[i]=array[i];
-//            }
-//            this.array=newArray;
-//            nextFirst=newArray.length-1;
-//            nextLast=size;
-//        }
-//    }
-private void resize(){
-    T[] newArray=(T[]) new Object[array.length*2];
-    int p=moveLoop(true,nextFirst);
-    for (int i=0;i<size;i++){
-        newArray[i]=this.array[p];
-        p=moveLoop(true,p);
+
+private void resize() {
+    T[] newArray = (T[]) new Object[array.length*2];
+    int p = moveLoop(true,nextFirst);
+    for (int i=0;i<size;i++) {
+        newArray[i] = this.array[p];
+        p = moveLoop(true,p);
     }
-    this.array=newArray;
-    nextFirst=newArray.length-1;
-    nextLast=size;
+    this.array = newArray;
+    nextFirst = newArray.length-1;
+    nextLast = size;
 }
-//    private void resizeShort(){
-//        T[] newArray=(T[]) new Object[array.length/2];
-//        for(int i=0;i<nextLast;i++){
-//            newArray[i]=array[i];
-//        }
-//        for(int i=0;i<array.length-1-nextFirst;i++){
-//            newArray[newArray.length-i]=array[array.length-i];
-//        }
-//        nextFirst=newArray.length-1-(array.length-1-nextFirst);
-//        this.array=newArray;
-//    }
-    private void resizeShort(){
-        T[] newArray=(T[]) new Object[array.length/2];
-        int p=moveLoop(true,nextFirst);
-        for (int i=0;i<size;i++){
-            newArray[i]=this.array[p];
-            p=moveLoop(true,p);
+
+    private void resizeShort() {
+        T[] newArray = (T[]) new Object[array.length/2];
+        int p = moveLoop(true,nextFirst);
+        for (int i = 0;i < size;i++){
+            newArray[i] = this.array[p];
+            p = moveLoop(true,p);
         }
-        this.array=newArray;
-        nextFirst=newArray.length-1;
-        nextLast=size;
+        this.array = newArray;
+        nextFirst = newArray.length-1;
+        nextLast = size;
     }
-    private int moveLoop(Boolean dir,int pos){
+    private int moveLoop(Boolean dir,int pos) {
         if(dir){
-            if(pos< array.length-1){
+            if(pos < array.length-1) {
                 pos++;
                 return pos;
             }
             return 0;
         }
         else {
-            if(pos>0){
+            if(pos > 0){
                 pos--;
                 return pos;
             }
             return array.length-1;
         }
     }
-    public int size(){
+    public int size() {
         return size;
     }
-    public void printDeque(){
-        int pos=moveLoop(true,nextFirst);
-        for (int i=0;i<size;i++){
+    public void printDeque() {
+        int pos = moveLoop(true,nextFirst);
+        for (int i = 0;i<size;i++){
             System.out.print(array[pos]+" ");
-            pos=moveLoop(true,pos);
+            pos = moveLoop(true,pos);
         }
         System.out.println(" ");
     }
-    public T get(int i){
-        int head=array.length-1-nextFirst;
-        if (i<head){
+    public T get(int i) {
+        int head = array.length-1-nextFirst;
+        if (i<head) {
             return array[nextFirst+1+i];
         }
         else {
             return array[i-head];
         }
     }
-    public boolean equals(Object o){
-        if(o instanceof Deque){
+    public boolean equals(Object o) {
+        if(o instanceof Deque) {
 //            int pos=moveLoop(true,nextFirst);
-            if (((Deque<?>) o).size()!=size){
+            if (((Deque<?>) o).size() != size) {
                 return false;
             }
-            for (int i=0;i<size;i++){
-                if(this.get(i).equals(((Deque<?>) o).get(i))==false){
+            for (int i=0;i<size;i++) {
+                if(this.get(i).equals(((Deque<?>) o).get(i)) == false) {
                     return false;
                 }
 //                pos=moveLoop(true,pos);
@@ -167,22 +137,22 @@ private void resize(){
         return false;
     }
 
-    private class ArrayDequeIterator implements Iterator<T>{
+    private class ArrayDequeIterator implements Iterator<T> {
         private int wizpos;
-        public ArrayDequeIterator(){
-            wizpos=0;
+        public ArrayDequeIterator() {
+            wizpos = 0;
         }
-        public boolean hasNext(){
-            return wizpos<size;
+        public boolean hasNext() {
+            return wizpos < size;
         }
-        public T next(){
-            T returnItem=get(wizpos);
-            wizpos+=1;
+        public T next() {
+            T returnItem = get(wizpos);
+            wizpos += 1;
             return returnItem;
         }
     }
 
-    public Iterator<T> iterator(){
+    public Iterator<T> iterator() {
         return new ArrayDequeIterator();
     }
 }
